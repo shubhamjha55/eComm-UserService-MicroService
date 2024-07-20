5364c6e4a1b0efb0b9ce9b11ead0c778adf3c546
Shubham Jha <shubham55jha@gmail.com>
2024-07-21 00:37:43 +0530

    Initial Commit

---
+++ src/main/java/com/example/user_service/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.Entity;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
